ec4c171b3ccf68eb2c55957a500f98cbea4a7f2f
darey6270 <darey6270@gmail.com>
2024-11-09 13:24:24 -0800

    change server to inde

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ const userRoute = require("./routes/userRoute");
 const errorHandler = require("./middleWare/errorMiddleware");
 const cookieParser = require("cookie-parser");
 const path = require("path");
+
 // Referral routes
 const referralRoutes = require('./routes/referralRoute');
 const randomRoutes = require('./routes/randomRoute');
+
 const app = express();
 
 // Middlewares
@@ -17,12 +19,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// app.use(
-//   cors({
-//     origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
-//     credentials: true,
-//   })
-// );
 
 app.use(cors());
 
@@ -37,7 +33,6 @@ app.use('/api/posters', require('./controllers/posterController'));
 app.use('/api/referrals', referralRoutes);
 app.use('/api/random', randomRoutes);
 
-
 // Routes
 app.get("/", (req, res) => {
   res.send("Home Page");
@@ -45,11 +40,12 @@ app.get("/", (req, res) => {
 
 // Error Middleware
 app.use(errorHandler);
-// Connect to DB and start server
-const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.MONGO_URI).then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server Running on port ${PORT}`);
-    });
-  })
-  .catch((err) => console.log(err));
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("MongoDB connection error:", err));
+
+// Export the Express app for Vercel
+module.exports = app;
